0a6c727548097b01751936c3796885acb0f7eb2d
VicenteVc <42993663+VicenteVc@users.noreply.github.com>
2019-05-04 09:06:48 +0800

    Update survey.js

--- survey.js
+++ survey.js
@@ -3,7 +3,7 @@ var isPilot = true;
 var isPrivacy = (Math.random() > 0.5);
 var isIntel = Boolean(1 - isPrivacy);
 //var condIndex = Math.floor(Math.random() * 10);
-var condIndex = Math.floor(Math.random() * 3);        /* Strong blur level condition */
+var condIndex = Math.floor(Math.random() * 3) + 4;        /* Strong blur level condition */
 if (condIndex == 3 || condIndex == 7) condIndex--;
 
 var imgCond = parseInt(condIndex / 4);
